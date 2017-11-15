47358aca078f6d97bf643c5163140223a742988a
Jorge Izquierdo <izqui97@gmail.com>
2017-11-15 10:25:17 +0100

    Merge pull request #16 from aragon/ens-address
    Add ENS registry address flag

--- packages/aragon-cli/src/handle.js
+++ packages/aragon-cli/src/handle.js
@@ -26,12 +26,8 @@ const handlers = {
       reporter.fatal('No name specified')
     }
 
-    if (!flags.registry) {
-      reporter.fatal('No registry specified')
-    }
-
     pkg.write({
-      appName: name + '.' + flags.registry,
+      appName: name,
       version: '1.0.0',
       roles: [],
       path: 'src/App.sol'
@@ -44,7 +40,8 @@ const handlers = {
       .then(async ({ appName }) => {
         const eth = new Web3Eth(flags.rpc)
 
-        const repoAddress = await ens.resolve(appName, eth, flags.chainId)
+        const ensAddress = flags.ensRegistry || ens.chainRegistry(flags.chainId)
+        const repoAddress = await ens.resolve(appName, eth, ensAddress)
         if (repoAddress === consts.NULL_ADDRESS) {
           return []
         }
@@ -200,11 +197,12 @@ const handlers = {
       .then(async ({ appName, version, hash }) => {
         const eth = new Web3Eth(flags.rpc)
 
-        let repoAddress = await ens.resolve(appName, eth, flags.chainId)
+        const ensAddress = flags.ensRegistry || ens.chainRegistry(flags.chainId)
+        let repoAddress = await ens.resolve(appName, eth, ensAddress)
         if (repoAddress === consts.NULL_ADDRESS) {
           // Create new repo
           const registryName = appName.split('.').splice(-2).join('.')
-          const registryAddress = await ens.resolve(registryName, eth, flags.chainId)
+          const registryAddress = await ens.resolve(registryName, eth, ensAddress)
           if (registryAddress === consts.NULL_ADDRESS) {
             reporter.error(`Registry ${registryName} does not exist.`)
             return
