c02af3251e1708f9720d8432dee3b2a5763baebc
Oliver Nordbjerg <hi@notbjerg.me>
2017-11-20 16:16:06 +0100

    Fix typo in `versions` command

--- src/commands/versions.js
+++ src/commands/versions.js
@@ -6,14 +6,14 @@ exports.command = 'versions'
 
 exports.describe = 'List all versions of the package'
 
-exports.handler = async function (reporter, { bump, cwd, web3 }) {
+exports.handler = async function (reporter, { bump, cwd, ethRpc }) {
   const moduleLocation = await findUp('module.json', { cwd })
   if (!moduleLocation) {
     throw new MessageError('This directory is not an Aragon project',
   'ERR_NOT_A_PROJECT')
   }
 
-  return apm(web3).getAllVersions(module.appName)
+  return apm(ethRpc).getAllVersions(module.appName)
     .then((versions) => {
       versions.forEach((version) => {
         const contentURI = Buffer.from(version.contentURI.substring(2), 'hex').toString('ascii')
