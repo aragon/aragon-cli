d1af67f6b10fec39a5feb2421ddc166b56007bab
Oliver Nordbjerg <hi@notbjerg.me>
2017-12-04 16:21:05 +0100

    Use `Error.captureStackTrace` in `MessageError`

--- src/errors.js
+++ src/errors.js
@@ -1,9 +1,8 @@
 exports.MessageError = class MessageError extends Error {
   constructor (msg, code) {
     super(msg)
-    this.code = code
 
-    // Ugly hack to make `instanceof` work
-    Object.setPrototypeOf(this, MessageError.prototype)
+    this.code = code
+    Error.captureStackTrace(this, MessageError)
   }
 }
