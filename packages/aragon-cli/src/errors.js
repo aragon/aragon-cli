49ba5a7064fc9cd1c4226187484372041968c58d
Oliver <onbjerg@users.noreply.github.com>
2018-04-07 10:14:08 +0200

    Merge pull request #52 from aragon/cleanup-reporter
    Clean up reporter

--- packages/aragon-cli/src/errors.js
+++
@@ -1,8 +0,0 @@
-exports.MessageError = class MessageError extends Error {
-  constructor (msg, code) {
-    super(msg)
-
-    this.code = code
-    Error.captureStackTrace(this, MessageError)
-  }
-}
