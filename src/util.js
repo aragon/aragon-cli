af7aa5b832994b6ff926a49b0d98321ce786c85f
Luis Cuende <me@luisivan.net>
2018-05-15 15:35:25 +0200

    Fix findProjectRoot bug causing dao commands to throw

--- src/util.js
+++ src/util.js
@@ -10,7 +10,12 @@ let cachedNodePackageManager
 
 const findProjectRoot = () => {
   if (!cachedProjectRoot) {
-    cachedProjectRoot = path.dirname(findUp.sync('arapp.json'))
+    try {
+      cachedProjectRoot = path.dirname(findUp.sync('arapp.json'))
+    } catch (_) {
+      console.error('This directory is not an Aragon project')
+      process.exit(1)
+    }
   }
   return cachedProjectRoot
 }
