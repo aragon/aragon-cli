5976069c5e8ee88c40770df892139694e6a2e168
Jorge Izquierdo <izqui97@gmail.com>
2018-07-10 12:13:38 +0200

    Merge pull request #155 from jtremback/network-id-fix
    janky network id fix

--- packages/aragon-cli/src/commands/devchain.js
+++ packages/aragon-cli/src/commands/devchain.js
@@ -54,7 +54,8 @@ exports.task = async function ({ port = 8545, reset = false, showAccounts = 2 })
       const server = ganache.server({
         gasLimit: BLOCK_GAS_LIMIT,
         mnemonic: MNEMONIC,
-        db_path: snapshotPath
+        db_path: snapshotPath,
+        network_id: 15, // aragen uses network_id 15 when creating snapshot https://github.com/aragon/aragen/blob/3df0d65a9de7bbdeeea763a444e0ab367db366a0/scripts/start-ganache#L6
       })
       const listen = () => (
         new Promise((resolve, reject) => {
