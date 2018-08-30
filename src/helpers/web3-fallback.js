567c0dab6c388b855ddc7b5677602f130d52496e
Jorge Izquierdo <jorge@izqui.me>
2018-08-30 15:29:36 +0200

    Add link to truffle configuration for provider configuration

--- src/helpers/web3-fallback.js
+++ src/helpers/web3-fallback.js
@@ -8,7 +8,11 @@ const ensureWeb3 = async (network) => {
     const connected = await web3.eth.net.isListening()
     if (connected) return web3
   } catch (err) {
-    throw new Error(`Web3 cannot connect using the network provider. Make sure 'aragon devchain' is running or your provider settings are correct.`)
+    throw new Error(`Web3 cannot connect using the network provider.
+
+Make sure 'aragon devchain' is running or your provider settings are correct.
+
+For more info you can check the Truffle docs on network configuration: https://truffleframework.com/docs/truffle/reference/configuration#networks`)
   }
 }
 
