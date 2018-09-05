e1ea4fcef6572140fe9db709a0b9e7e556aa2248
Jorge Izquierdo <izqui97@gmail.com>
2018-09-05 11:27:39 +0200

    Implement ACL write commands (#190)
    * dao acl view: fix truncated proxy addresses
    
    * dao exec: extract exec logic into handler for reusability
    
    * aragonjs-wrapper: clean up unused code
    
    * dao acl grant: Implement command
    
    * dao acl create: Implement command
    
    * dao acl revoke: Implement command
    
    * dao acl set-manager: Implement command
    
    * dao acl remove-manager: Implement command
    
    * Exec handler clean up
    
    * dao acl: allow role names
    
    * Update to @aragon/wrapper 2.0.0-beta.43
    
    * Fix wrapper connection ipfs settings
    
    * Update package-lock.json

--- packages/aragon-cli/src/commands/dao_cmds/utils/aragonjs-wrapper.js
+++ packages/aragon-cli/src/commands/dao_cmds/utils/aragonjs-wrapper.js
@@ -5,26 +5,8 @@ import Aragon, {
   isNameUsed,
   ensResolve,
 } from '@aragon/wrapper'
-import {
-  appOverrides,
-  sortAppsPair,
-  appLocator,
-  ipfsDefaultConf,
-} from './environment'
-
 
 const noop = () => {}
-
-const appSrc = (app, gateway = ipfsDefaultConf.gateway) => {
-  const hash = app.content && app.content.location
-  if (!hash) return ''
-
-  if (appLocator[app.appId]) {
-    return appLocator[app.appId]
-  }
-
-  return `${gateway}/${hash}/`
-}
 
 // Subscribe to wrapper's observables
 const subscribe = (
@@ -63,7 +45,7 @@ const initWrapper = async (
     provider,
     accounts = '',
     walletProvider = null,
-    ipfsConf = ipfsDefaultConf,
+    ipfsConf = {},
     onError = noop,
     onApps = noop,
     onForwarders = noop,
