9ed6b4e32fb03a60ac7e1525022fc7a13a28ba31
perissology <perissology@protonmail.com>
2018-10-25 15:15:15 -0700

    add apm info & packages cmds

---
+++ src/commands/apm_cmds/info.js
@@ -0,0 +1,35 @@
+const APM = require('@aragon/apm')
+const chalk = require('chalk')
+const TaskList = require('listr')
+const defaultAPMName = require('../../helpers/default-apm')
+const { ensureWeb3 } = require('../../helpers/web3-fallback')
+const getRepoTask = require('../dao_cmds/utils/getRepoTask')
+
+exports.command = 'info <apmRepo> [apmRepoVersion]'
+
+exports.describe = 'Get information about a package'
+
+exports.builder = getRepoTask.args
+
+exports.handler = async function ({ apmRepo, apm: apmOptions, apmRepoVersion, network }) {
+  const web3 = await ensureWeb3(network)
+  apmRepo = defaultAPMName(apmRepo)
+  apmOptions.ensRegistryAddress = apmOptions['ens-registry']
+  const apm = await APM(web3, apmOptions)
+
+  const tasks = new TaskList([
+    {
+      title: `Fetching ${chalk.bold(apmRepo)}@${apmRepoVersion}`,
+      task: getRepoTask.task({ apm, apmRepo, apmRepoVersion, artifactRequired: false })
+    }
+  ])
+
+  return tasks.run()
+    .then((ctx) => {
+      delete ctx.repo.abi
+      delete ctx.repo.environments
+
+      console.log(JSON.stringify(ctx.repo, null, 2))
+      process.exit()
+    })
+}
