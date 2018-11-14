f94f9daf93da7d642e9672c2b6a47c78449f50c2
Daniel Constantin <hello@danielconstantin.net>
2018-11-14 13:30:50 +0100

    style: fix linting

--- src/helpers/listr-options.js
+++ src/helpers/listr-options.js
@@ -4,15 +4,15 @@ const ListrRenderer = require('../reporters/ListrRenderer')
  * https://github.com/SamVerschueren/listr#options
  * https://github.com/SamVerschueren/listr-update-renderer#options
  * https://github.com/SamVerschueren/listr-verbose-renderer#options
- * 
- * @param {boolean} silent 
+ *
+ * @param {boolean} silent
  * @param {boolean} debug
- * @returns {Object} listr options object 
+ * @returns {Object} listr options object
  */
 function listrOpts (silent, debug) {
   return {
     renderer: ListrRenderer(silent, debug),
-    dateFormat: false,
+    dateFormat: false
   }
 }
 
