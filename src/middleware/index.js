ffdb1e9006f79728c78bc48407e792f0088659fa
galactusss <gabrielpk.18@gmail.com>
2019-01-04 21:36:35 -0300

    delete old root files

--- src/middleware/index.js
+++
@@ -1,9 +0,0 @@
-const manifestMiddleware = require('./manifest')
-const moduleMiddleware = require('./module')
-const environmentMiddleware = require('./environment')
-
-module.exports = {
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-}
