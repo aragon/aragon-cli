bbafea9590ebcca8aa06379a270de09da6115ca2
Daniel Constantin <hello@danielconstantin.net>
2019-01-06 11:15:19 +0200

    Handle arapp.json parse errors

--- src/middleware/module.js
+++ src/middleware/module.js
@@ -9,6 +9,20 @@ module.exports = function moduleMiddleware(argv) {
       const modulePath = path.resolve(findProjectRoot(), 'arapp.json')
       const arapp = fs.readJsonSync(modulePath)
 
+      const Ajv = require('ajv')
+      const arappSchema = require('../../schemas/arapp.schema')
+      const ajv = new Ajv({ allErrors: true })
+      const validate = ajv.compile(arappSchema)
+      const valid = validate(arapp)
+
+      if (!valid) {
+        const errors = ajv.errorsText(validate.errors, {
+          dataVar: 'arapp',
+        })
+        argv.reporter.error(`Error parsing the aragon config file: ${errors}!`)
+        process.exit(1)
+      }
+
       // hack: we need to access the module in downstream middleware, but
       // yargs does not update the `argv` param until all middleware have
       // ran, so we directly mutate the `argv` param
