b7563fffd97530d7e060c986a841b1987e196835
galactusss <gabrielpk.18@gmail.com>
2019-01-17 19:14:01 -0300

    remove middlewares from token and dao commands

--- src/commands/dao_cmds/token.js
+++ src/commands/dao_cmds/token.js
@@ -1,26 +1,9 @@
-const {
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-} = require('../../middleware')
-
-const MIDDLEWARES = [
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-]
-
 exports.command = 'token <command>'
 
 exports.describe = 'Create and interact with MiniMe tokens'
 
 exports.builder = function(yargs) {
-  return yargs
-    .commandDir('token_cmds', {
-      visit: cmd => {
-        cmd.middlewares = MIDDLEWARES
-        return cmd
-      },
-    })
-    .demandCommand(1, 'You need to specify a command')
+  const cmd = yargs.commandDir('token_cmds')
+  cmd.demandCommand(1, 'You need to specify a command')
+  return cmd
 }
