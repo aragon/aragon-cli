77a56019365799e6c37fe8a843fca6b8a058002c
Gabriel Garcia <gabrielpk.18@gmail.com>
2019-03-05 07:00:33 -0300

    Yargs12 (#386)

--- packages/aragon-cli/src/commands/apm.js
+++ packages/aragon-cli/src/commands/apm.js
@@ -1,15 +1,3 @@
-const {
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-} = require('../middleware')
-
-const MIDDLEWARES = [
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-]
-
 exports.command = 'apm <command>'
 
 exports.describe = 'Publish and manage your APM package'
@@ -17,14 +5,7 @@ exports.describe = 'Publish and manage your APM package'
 exports.aliases = ['package']
 
 exports.builder = function(yargs) {
-  const cmd = yargs.commandDir('apm_cmds', {
-    visit: cmd => {
-      // Add middlewares
-      cmd.middlewares = MIDDLEWARES
-      return cmd
-    },
-  })
-  cmd.demandCommand(1, 'You need to specify a command')
-
-  return cmd
+  return yargs
+    .commandDir('apm_cmds')
+    .demandCommand(1, 'You need to specify a command')
 }
--- packages/aragon-cli/src/commands/dao_cmds/token.js
+++ packages/aragon-cli/src/commands/dao_cmds/token.js
@@ -1,26 +1,9 @@
-const {
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-} = require('../../middleware')
-
-const MIDDLEWARES = [
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
-]
-
 exports.command = 'token <command>'
 
 exports.describe = 'Create and interact with MiniMe tokens'
 
 exports.builder = function(yargs) {
   return yargs
-    .commandDir('token_cmds', {
-      visit: cmd => {
-        cmd.middlewares = MIDDLEWARES
-        return cmd
-      },
-    })
+    .commandDir('token_cmds')
     .demandCommand(1, 'You need to specify a command')
 }
