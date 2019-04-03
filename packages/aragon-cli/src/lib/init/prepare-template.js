edb9a2198d2a69b08be5f295b41780a641df374e
Daniel Constantin <hello@danielconstantin.net>
2019-04-03 16:22:39 +0300

    Improve prepareTemplate

--- packages/aragon-cli/src/lib/init/prepare-template.js
+++ packages/aragon-cli/src/lib/init/prepare-template.js
@@ -1,32 +1,59 @@
 import path from 'path'
 import fs from 'fs-extra'
+import replace from 'replace'
 
+/**
+ * Remove the `.git` dir, licenses and rename the app name and registry.
+ * (It will search and replace anything that matches `placeholder-app-name`)
+ *
+ * @param {string} dir - for example `foo`
+ * @param {string} appName - for example `foo.myApmRegistry.eth`
+ */
 export async function prepareTemplate(dir, appName) {
-  const basename = appName.split('.')[0]
-  const arappPath = path.resolve(dir, 'arapp.json')
-  const arapp = await fs.readJson(arappPath)
-
-  const defaultEnv = arapp.environments.default
-  const stagingEnv = arapp.environments.staging
-  const productionEnv = arapp.environments.production
-
-  defaultEnv.appName = appName
-  stagingEnv.appName = stagingEnv.appName.replace(/^app/, basename)
-  productionEnv.appName = productionEnv.appName.replace(/^app/, basename)
-
-  Object.assign(arapp.environments.default, defaultEnv)
-  Object.assign(arapp.environments.staging, stagingEnv)
-  Object.assign(arapp.environments.production, productionEnv)
-
+  /**
+   * Delete .git
+   */
   const gitFolderPath = path.resolve(dir, '.git')
-  const licensePath = path.resolve(dir, 'LICENSE')
 
+  /**
+   * Delete licenses
+   */
+  const licensePath = path.resolve(dir, 'LICENSE')
   const packageJsonPath = path.resolve(dir, 'package.json')
   const packageJson = await fs.readJson(packageJsonPath)
   delete packageJson.license
 
+  /**
+   * Replace the default registries with the preferred one
+   */
+  const defaultRegistries = [
+    // To keep backwards compability, only change `aragonpm.eth`, leave `open.aragonpm.eth`
+    'placeholder-app-name.aragonpm.eth',
+    // 'placeholder-app-name.open.aragonpm.eth',
+  ]
+
+  defaultRegistries.forEach(registry => {
+    replace({
+      regex: registry,
+      replacement: appName,
+      paths: [dir],
+      recursive: true,
+      silent: true,
+    })
+  })
+
+  /**
+   * Replace the placeholder name with the actual name
+   */
+  replace({
+    regex: 'placeholder-app-name',
+    replacement: dir,
+    paths: [dir],
+    recursive: true,
+    silent: true,
+  })
+
   return Promise.all([
-    fs.writeJson(arappPath, arapp, { spaces: 2 }),
     fs.writeJson(packageJsonPath, packageJson, { spaces: 2 }),
     fs.remove(gitFolderPath),
     fs.remove(licensePath),
