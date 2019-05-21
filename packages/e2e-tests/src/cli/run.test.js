3269976733ad3a6550a3eea22c84aa4240ab7450
Daniel Constantin <hello@danielconstantin.net>
2019-05-21 15:17:05 +0300

    Update test

--- packages/e2e-tests/src/cli/run.test.js
+++ packages/e2e-tests/src/cli/run.test.js
@@ -19,34 +19,40 @@ test('should create a new aragon app and run it successfully', async t => {
 
   // arrange
   const projectName = 'foobarfoo'
+  await execa('create-aragon-app', [projectName], { cwd: testSandbox })
+  // hack, we need to install the dependencies of the app
+  await execa('npm', ['install'], { cwd: `${testSandbox}/${projectName}/app` })
 
   // act
-  await execa('create-aragon-app', [projectName], { cwd: testSandbox })
   const runProcess = await startBackgroundProcess({
     cmd: 'aragon',
-    args: ['run', '--debug'],
+    args: ['run', '--debug', '--reset'],
     execaOpts: { cwd: `${testSandbox}/${projectName}` },
     readyOutput: 'Opening http://localhost:3000/#/',
   })
-
-  // hack, we need to install the dependencies of the app
-  await execa('npm', ['install'], { cwd: `${testSandbox}/${projectName}/app` })
 
   // hack so the wrapper has time to start
   await new Promise(resolve => setTimeout(resolve, 2 * 60 * 1000))
 
   // finding the DAO address
   const daoAddress = runProcess.stdout.match(/DAO address: (0x[a-fA-F0-9]{40})/)[1]
-  
-  // TODO fetch the counter app
+
+  // TODO fetch the counter app instead
   const fetchResult = await fetch(`http://localhost:3000/#/${daoAddress}`)
   const fetchBody = await fetchResult.text()
-  
+
   // cleanup
   await runProcess.exit()
 
+  // delete the output part of building the frontend
+  const appBuildOutput = runProcess.stdout.substring(
+    runProcess.stdout.indexOf('Building frontend [started]'),
+    runProcess.stdout.indexOf('Building frontend [completed]')
+  )
+  const outputToSnapshot = runProcess.stdout.replace(appBuildOutput, '')
+
   // assert
-  t.snapshot(normalizeOutput(runProcess.stdout))
+  t.snapshot(normalizeOutput(outputToSnapshot))
   t.snapshot(fetchResult.status)
   t.snapshot(fetchBody)
 })
