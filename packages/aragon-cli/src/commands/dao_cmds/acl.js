7f037808889ff449e43f339ebbfc544c7d484619
0xGabi <gabrielpk.18@gmail.com>
2019-07-06 20:45:16 -0300

    v6.0.1

--- packages/aragon-cli/src/commands/dao_cmds/acl.js
+++ packages/aragon-cli/src/commands/dao_cmds/acl.js
@@ -2,7 +2,8 @@ const viewCommand = require('./acl_cmds/view')
 
 exports.command = 'acl <dao>'
 
-exports.describe = 'Shortcut for aragon dao acl view <dao>'
+exports.describe =
+  'View and manage your DAO permissions. Shortcut for aragon dao acl view <dao>'
 
 exports.builder = function(yargs) {
   return yargs.commandDir('acl_cmds')
