c5ce596655faddf2d19ff4d6e36eb5b58b5d6d3e
0xGabi <gabrielpk.18@gmail.com>
2019-07-20 17:56:05 -0300

    Fix import

--- packages/aragon-cli/test/commands/init.test.js
+++ packages/aragon-cli/test/commands/init.test.js
@@ -4,7 +4,7 @@ import fs from 'fs-extra'
 import { checkProjectExists, prepareTemplate } from '../../src/lib/init'
 import { isValidAragonId } from '../../src/util'
 
-import defaultAPMName from '../../src/helpers/default-apm'
+import defaultAPMName from '@aragon/cli-utils/src/helpers/default-apm'
 
 const projectPath = './.tmp/aragon-app'
 
