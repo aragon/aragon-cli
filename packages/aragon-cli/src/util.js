ded0bb260f6c4d819cb28835e1609ac810d497f4
Mathew Cormier <mathew.corm@gmail.com>
2019-09-24 20:31:20 -0400

    Move addressesEqual to util.js and remove unused functions

--- packages/aragon-cli/src/util.js
+++ packages/aragon-cli/src/util.js
@@ -13,6 +13,17 @@ const debugLogger = process.env.DEBUG ? console.log : () => {}
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
+/**
+ * Check eth address equality without checksums
+ * @param {string} first address
+ * @param {string} second address
+ */
+function addressesEqual(first, second) {
+  first = first && first.toLowerCase()
+  second = second && second.toLowerCase()
+  return first === second
+}
+
 const findProjectRoot = () => {
   if (!cachedProjectRoot) {
     try {
@@ -281,6 +292,7 @@ function isValidAragonId(aragonId) {
 }
 
 module.exports = {
+  addressesEqual,
   parseArgumentStringIfPossible,
   debugLogger,
   findProjectRoot,
