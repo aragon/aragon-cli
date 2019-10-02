962ec6dd29602815f715285b49b0b385b1ad92d7
Mathew Cormier <mathew.corm@gmail.com>
2019-10-02 09:44:38 -0400

    Add params option to acl grant

--- packages/aragon-cli/src/commands/dao_cmds/acl_cmds/grant.js
+++ packages/aragon-cli/src/commands/dao_cmds/acl_cmds/grant.js
@@ -1,15 +1,23 @@
 const daoArg = require('../utils/daoArg')
 const aclExecHandler = require('./utils/aclExecHandler')
+const { convertStringToParam, encodeParam } = require('./utils/params')
+const { isArray } = require('lodash')
 
 // Note: we usually order these values as entity, proxy, role but this order fits
 //       better with other CLI commands
-exports.command = 'grant <dao> <app> <role> <entity>'
+exports.command = 'grant <dao> <app> <role> <entity> [params]'
 
 exports.describe =
   'Grant a permission in a DAO (only permission manager can do it)'
 
 exports.builder = function(yargs) {
   return daoArg(yargs)
+    .option('params', {
+      description: 'Parameters',
+      type: 'array',
+      default: [],
+      coerce: params => isArray(params) ? params : [params]
+    })
 }
 
 exports.handler = async function({
@@ -22,15 +30,25 @@ exports.handler = async function({
   wsProvider,
   apm,
   gasPrice,
+  params
 }) {
-  const method = 'grantPermission'
-  const params = [entity, app, role]
-  return aclExecHandler(dao, method, params, {
+  const method = params.length === 0
+    ? 'grantPermission'
+    : 'grantPermissionP'
+
+  const methodParams = params.length === 0 
+    ? [entity, app, role]
+    : [entity, app, role, params.map(convertStringToParam).map(encodeParam)]
+
+  return aclExecHandler(dao, method, methodParams, {
     reporter,
     gasPrice,
     apm,
     network,
     wsProvider,
-    role,
+    role
   })
 }
+
+
+
