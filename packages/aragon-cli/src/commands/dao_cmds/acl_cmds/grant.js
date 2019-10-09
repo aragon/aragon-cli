ce9725985dba57d403cdf6e790c38382d73c823e
Mathew Cormier <mathew.corm@gmail.com>
2019-10-08 20:00:04 -0400

    Support ACL permissions with parameters (#807)

--- packages/aragon-cli/src/commands/dao_cmds/acl_cmds/grant.js
+++ packages/aragon-cli/src/commands/dao_cmds/acl_cmds/grant.js
@@ -1,15 +1,19 @@
 const daoArg = require('../utils/daoArg')
 const aclExecHandler = require('./utils/aclExecHandler')
+const { convertStringToParam, encodeParam } = require('./utils/params')
 
 // Note: we usually order these values as entity, proxy, role but this order fits
 //       better with other CLI commands
-exports.command = 'grant <dao> <app> <role> <entity>'
+exports.command = 'grant <dao> <app> <role> <entity> [params...]'
 
 exports.describe =
   'Grant a permission in a DAO (only permission manager can do it)'
 
 exports.builder = function(yargs) {
-  return daoArg(yargs)
+  return daoArg(yargs).positional('params', {
+    description: 'ACL parameters',
+    default: [],
+  })
 }
 
 exports.handler = async function({
@@ -22,10 +26,16 @@ exports.handler = async function({
   wsProvider,
   apm,
   gasPrice,
+  params,
 }) {
-  const method = 'grantPermission'
-  const params = [entity, app, role]
-  return aclExecHandler(dao, method, params, {
+  const method = params.length === 0 ? 'grantPermission' : 'grantPermissionP'
+
+  const methodParams =
+    params.length === 0
+      ? [entity, app, role]
+      : [entity, app, role, params.map(convertStringToParam).map(encodeParam)]
+
+  return aclExecHandler(dao, method, methodParams, {
     reporter,
     gasPrice,
     apm,
