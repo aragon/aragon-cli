b291e241a78e58d19e84678cd6667d7f3e168946
Gabriel Garcia <gabi@1hive.org>
2019-10-11 13:02:53 +0900

    Remove version alias to prevent clash with devchain

--- packages/aragon-cli/src/cli.js
+++ packages/aragon-cli/src/cli.js
@@ -35,7 +35,6 @@ cmd.middleware(MIDDLEWARES)
 
 cmd.alias('env', 'environment')
 cmd.alias('h', 'help')
-cmd.alias('v', 'version')
 
 // blank scriptName so that help text doesn't display "aragon" before each command
 cmd.scriptName('')
