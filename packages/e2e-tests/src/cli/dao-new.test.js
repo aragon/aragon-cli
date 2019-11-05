d3de2564d8a874a5138ad90d77a5fce1e5d79366
Gabriel Garcia <gabrielpk.18@gmail.com>
2019-11-05 14:06:16 -0500

    Fix e2e-test on master (#855)

--- packages/e2e-tests/src/cli/dao-new.test.js
+++ packages/e2e-tests/src/cli/dao-new.test.js
@@ -10,10 +10,8 @@ test('creates a new DAO', async t => {
 
   const result = await execa('aragon', ['dao', 'new', '--debug'])
   const daoAddress = result.stdout.match(daoAddressRegex)[1]
-  
-  const resultSnapshot = normalizeOutput(result.stdout)
-    .replace(daoAddress, '')
-  
+
+  const resultSnapshot = normalizeOutput(result.stdout).replace(daoAddress, '')
 
   t.assert(/0x[a-fA-F0-9]{40}/.test(daoAddress), 'Invalid DAO address')
   t.snapshot(resultSnapshot)
@@ -22,13 +20,17 @@ test('creates a new DAO', async t => {
 test('assigns an Aragon Id with the "--aragon-id" param', async t => {
   t.plan(3)
 
-  const result = await execa('aragon', ['dao', 'new', '--debug', '--aragon-id', 'newdao1' ])
-  const [,daoId, daoAddress] = result.stdout.match(daoIdAndAddressAddressRegex)
-  
-  const resultSnapshot = normalizeOutput(result.stdout)
-    .replace(daoAddress, '') 
-  
- 
+  const result = await execa('aragon', [
+    'dao',
+    'new',
+    '--debug',
+    '--aragon-id',
+    'newdao1',
+  ])
+  const [, daoId, daoAddress] = result.stdout.match(daoIdAndAddressAddressRegex)
+
+  const resultSnapshot = normalizeOutput(result.stdout).replace(daoAddress, '')
+
   t.assert(daoId === `newdao1.aragonid.eth`, 'Invalid Aragon Id')
   t.assert(/0x[a-fA-F0-9]{40}/.test(daoAddress), 'Invalid DAO address')
   t.snapshot(resultSnapshot)
