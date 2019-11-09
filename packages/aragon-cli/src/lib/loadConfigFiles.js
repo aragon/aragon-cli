cf9d95f7a73ecebfca79ca10fcebf8d3365ae3f9
dapplion <guillem.lion.benosa@gmail.com>
2019-11-09 12:08:16 +0100

    Refactor middlewares

---
+++ packages/aragon-cli/src/lib/loadConfigFiles.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const fs = require('fs-extra')
+const { findProjectRoot } = require('../util')
+const Ajv = require('ajv')
+const arappSchema = require('../../schemas/arapp.schema')
+const ajv = new Ajv({ allErrors: true })
+
+/**
+ * Looks for a filename recursively up from the cwd
+ * Return null if the file is not found or can't be loaded
+ * @param {string} fileName
+ * @return {Object|null} Parsed file JSON content
+ */
+function loadJsonFileIfFound(fileName) {
+  try {
+    const filePath = path.resolve(findProjectRoot(), fileName)
+    return fs.readJsonSync(filePath)
+  } catch (e) {
+    return null
+  }
+}
+
+/**
+ * Loads the arapp.json file. If it's non existent, it returns null
+ * @return {Object|undefined}
+ */
+export function loadArappFile() {
+  const arapp = loadJsonFileIfFound('arapp.json')
+  if (!arapp) return null
+
+  const validate = ajv.compile(arappSchema)
+  const valid = validate(arapp)
+  if (!valid) {
+    const errors = ajv.errorsText(validate.errors, { dataVar: 'arapp' })
+    throw Error(`Error parsing the aragon config file: ${errors}!`)
+  }
+
+  return arapp
+}
+
+export function loadManifestFile() {
+  return loadJsonFileIfFound('manifest.json')
+}
--- packages/aragon-cli/src/middleware/index.js
+++ packages/aragon-cli/src/middleware/index.js
@@ -1,9 +1,69 @@
-const manifestMiddleware = require('./manifest')
-const moduleMiddleware = require('./module')
-const environmentMiddleware = require('./environment')
+const { loadManifestFile, loadArappFile } = require('../lib/loadConfigFiles')
+const { configEnvironment } = require('../lib/configEnvironment')
 
-module.exports = {
-  manifestMiddleware,
-  moduleMiddleware,
-  environmentMiddleware,
+export function configCliMiddleware(argv) {
+  const [cmd, subcmd] = argv._
+  const isInit = cmd === 'init'
+
+  if (isInit) return {}
+
+  try {
+    // Load config files
+
+    const manifest = loadManifestFile()
+    const arapp = loadArappFile()
+
+    // Configure environment (+ network)
+
+    if (!arapp)
+      argv.reporter.debug(
+        `Could not find 'arapp.json'. Using the default configuration`
+      )
+
+    const ignoreNetCmd = new Set(['version']) // i.e. 'aragon apm version'
+    const ignoreNetSubCmd = new Set(['init', 'devchain', 'ipfs', 'contracts']) // (TODO): remove init
+    const isTruffleFwd = cmd === 'contracts'
+    const ignoreNetwork = ignoreNetSubCmd.has(subcmd) || ignoreNetCmd.has(cmd)
+    const { useFrame, environment, network, apm } = argv
+
+    if (environment && network && !isTruffleFwd)
+      throw Error(
+        "Arguments '--network' and '--environment' are mutually exclusive. Using '--network'  has been deprecated and  '--environment' should be used instead."
+      )
+    if (network && arapp && arapp.environments && !isTruffleFwd)
+      throw Error(
+        "Your arapp.json contains an `environments` property. The use of '--network' is deprecated and '--environment' should be used instead."
+      )
+    if (arapp && !arapp.environments && environment)
+      throw Error(
+        "Your arapp.json does not contain an `environments` property. The use of '--environment'  is not supported."
+      )
+
+    const {
+      arapp: arappMutated,
+      apm: apmMutated,
+      network: networkObj,
+      wsProvider,
+    } = configEnvironment({
+      arapp,
+      ignoreNetwork,
+      useFrame,
+      isTruffleFwd,
+      environment,
+      network,
+      apm,
+    })
+
+    return {
+      manifest,
+      module: arappMutated,
+      apm: apmMutated,
+      network: networkObj,
+      wsProvider,
+    }
+  } catch (e) {
+    // Show errors prettier with the reporter
+    argv.reporter.error(e.message)
+    process.exit(1)
+  }
 }
