f0d653942cc992141593078576f2a6741b69a79a
Gabriel Garcia <gabrielpk.18@gmail.com>
2019-11-17 05:01:43 -0300

    Cosmetic fix

--- packages/aragon-cli/test/contracts/ParseMe.sol
+++ packages/aragon-cli/test/contracts/ParseMe.sol
@@ -43,9 +43,9 @@ contract ParseMe is IERC20 {
     }
 
     function transferFrom(
-    address from,
-    address to,
-    uint256 value
+        address from,
+        address to,
+        uint256 value
     )
         public
         returns (bool)
