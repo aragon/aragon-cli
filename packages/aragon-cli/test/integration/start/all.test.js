dad6398b5992841f2eaaaa7ae02bb4d962165da6
dapplion <guillem.lion.benosa@gmail.com>
2019-11-24 18:12:46 +0100

    Test start (placeholder only)
    f

---
+++ packages/aragon-cli/test/integration/start/all.test.js
@@ -0,0 +1,25 @@
+import test from 'ava'
+import { buildClient } from '../../../src/lib/start'
+
+/**
+ * Postpone until the refactor is completed
+ * The current code in `/lib` includes `listr` logic, which shouldn't happen
+ *
+ * - build-client.js (buildClient): Skipped
+ *   Would have to create a new package.json with a build:local script
+ *
+ * - download-client.js (downloadClient)
+ *
+ * - fetch-client.js (fetchClient)
+ *
+ * - open-client.js (openClient)
+ *
+ * - start-client.js (startClient)
+ *
+ */
+
+/* eslint-disable-next-line ava/no-skip-test */
+test.skip('Build client', async t => {
+  // arrange
+  await buildClient({}, '.')
+})
