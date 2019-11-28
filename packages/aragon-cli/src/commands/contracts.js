412e43d651ce9ba1c7f8599a0651b25f73a992a9
Mathew Cormier <mathew.corm@gmail.com>
2019-11-28 10:02:53 -0500

    Add contracts command builder

--- packages/aragon-cli/src/commands/contracts.js
+++ packages/aragon-cli/src/commands/contracts.js
@@ -2,7 +2,16 @@ const { contracts, extractTruffleArgs } = require('../lib/contracts')
 
 exports.command = 'contracts [command]'
 
-exports.describe = 'Execute any Truffle command with arguments'
+// yargs interprets `aragon contracts help` as its own help,
+// so advise users to type `aragon contracts` for Truffle's help.
+exports.describe =
+  'Execute any Truffle command with arguments. Type "aragon contracts" to display Truffle\'s help.'
+
+exports.builder = yargs => {
+  return yargs.positional('command', {
+    description: 'Truffle command',
+  })
+}
 
 exports.handler = async function({ reporter }) {
   reporter.info('Passing the command to Truffle')
