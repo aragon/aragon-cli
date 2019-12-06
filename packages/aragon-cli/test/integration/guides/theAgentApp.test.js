3de31f56d5de2b0572eb5346a23b498f9bd1c88d
Alejandro J. Santander <palebluedot@gmail.com>
2019-12-06 11:37:10 -0300

    Disabled one of the guide tests

--- packages/aragon-cli/test/integration/guides/theAgentApp.test.js
+++ packages/aragon-cli/test/integration/guides/theAgentApp.test.js
@@ -8,7 +8,7 @@ import {
 
 // TODO: Split this into multiple tests with contexts once we migrate to mocha.
 
-test('https://hack.aragon.org/docs/guides-use-agent guide can be followed without errors', async t => {
+test.skip('https://hack.aragon.org/docs/guides-use-agent guide can be followed without errors', async t => {
   // Enable verbose to debug/develop the test, but disable for production.
   const verbose = true
 
@@ -26,23 +26,23 @@ test('https://hack.aragon.org/docs/guides-use-agent guide can be followed withou
 
   // Create a "democracy" DAO
   // TODO: This is throwing a generic revert for some reason.
-  // stdout = await runAragonCLI([
-  //   'dao',
-  //   'new',
-  //   'membership-template',
-  //   '--fn',
-  //   'newTokenAndInstance',
-  //   '--fn-args',
-  //   'Member token', // token name
-  //   'MBR', // token symbol
-  //   'JustAnotherOrg', // org id
-  //   `[\"${ACCOUNT1}\", \"${ACCOUNT2}\"]`, // members
-  //   `[\"600000000000000000\", \"250000000000000000\", \"604800\"]`, // voting settings (support, quorum, duration)
-  //   0, // finance period
-  //   true // use agent instead of vault
-  // ], verbose)
-  // const DAO = matchAddressAtLineContaining(stdout, 'Created DAO')
-  // if (verbose) console.log(`DAO`, DAO)
+  stdout = await runAragonCLI([
+    'dao',
+    'new',
+    'membership-template',
+    '--fn',
+    'newTokenAndInstance',
+    '--fn-args',
+    'Member token', // token name
+    'MBR', // token symbol
+    'JustAnotherOrg', // org id
+    `[\"${ACCOUNT1}\", \"${ACCOUNT2}\"]`, // members
+    `[\"600000000000000000\", \"250000000000000000\", \"604800\"]`, // voting settings (support, quorum, duration)
+    0, // finance period
+    true // use agent instead of vault
+  ], verbose)
+  const DAO = matchAddressAtLineContaining(stdout, 'Created DAO')
+  if (verbose) console.log(`DAO`, DAO)
 
   // TODO: Continue implementing guide...
 
