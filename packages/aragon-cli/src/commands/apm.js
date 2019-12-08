dd1f6fbe4144ef649b95be2659587b806ba41635
Gabriel <gabrielpk.18@gmail.com>
2019-12-08 20:43:59 -0300

    Fix lint

--- packages/aragon-cli/src/commands/apm.js
+++ packages/aragon-cli/src/commands/apm.js
@@ -1,9 +1,9 @@
-export const command = 'apm <command>';
-export const describe = 'Publish and manage your aragonPM package';
-export const aliases = ['package'];
+export const command = 'apm <command>'
+export const describe = 'Publish and manage your aragonPM package'
+export const aliases = ['package']
 
 export const builder = function(yargs) {
   return yargs
     .commandDir('apm_cmds')
     .demandCommand(1, 'You need to specify a command')
-};
+}
