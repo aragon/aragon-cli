1d2f75377b41eb7f47f69dc048acd17d3e2f9aa8
Daniel Constantin <hello@danielconstantin.net>
2019-12-13 16:58:59 +0200

    Add kill to startProcess

--- packages/toolkit/src/node/process.js
+++ packages/toolkit/src/node/process.js
@@ -1,6 +1,22 @@
 import execa from 'execa'
+import { promisify } from 'util'
+import psTree from 'ps-tree'
 //
-import { withTimeout } from './misc'
+import { withTimeout, noop } from './misc'
+
+export const defaultKillSignal = 'SIGINT'
+
+export const getProcessTree = subprocess => promisify(psTree)(subprocess.pid)
+
+export const killProcessTree = async (subprocess, { logger = noop }) => {
+  const { children } = await getProcessTree(subprocess)
+
+  children.forEach(child => {
+    // each child has the properties: COMMAND, PPID, PID, STAT
+    logger(`killing process with PID ${child.pid}, child of ${subprocess.pid}`)
+    process.kill(child.PID, defaultKillSignal)
+  })
+}
 
 export const attachProcess = subprocess => {
   subprocess.stdout.pipe(process.stdout)
@@ -21,26 +37,30 @@ export const startProcess = async ({
   execaOpts = {},
   readyOutput,
   timeout,
+  logger = noop,
 }) => {
   let output = ''
 
   const request = new Promise((resolve, reject) => {
-    // start the process
     const subprocess = execa(cmd, args, execaOpts)
+    logger('spawned subprocess with PID: ', subprocess.pid)
 
     subprocess.stderr.on('data', data => {
       data = data.toString()
+      logger(`stderr: ${data}`)
       reject(new Error(data))
     })
 
     subprocess.stdout.on('data', data => {
       data = data.toString()
+      logger(`stdout: ${data}`)
       // build the output log (to be able to err out if the time is up)
       output = `${output}${data}\n`
       // check for ready signal
       if (data.includes(readyOutput)) {
         resolve({
           output,
+          kill: () => killProcessTree(subprocess, { logger }),
           attach: () => attachProcess(subprocess),
           detach: () => detachProcess(subprocess),
         })
