c74bd43bb63f65e3461a48c7478e20f63e4c76a7
Gabriel <gabrielpk.18@gmail.com>
2019-12-16 04:56:18 -0300

    Small changes

--- packages/aragon-cli/src/cli.js
+++ packages/aragon-cli/src/cli.js
@@ -2,13 +2,13 @@ import 'source-map-support/register'
 import yargs from 'yargs'
 import * as AragonReporter from './reporters/AragonReporter'
 const { toWei } = require('web3-utils')
+const url = require('url')
 
 const DEFAULT_GAS_PRICE = require('../package.json').aragon.defaultGasPrice
 
 const { configCliMiddleware } = require('./middleware')
 const { findProjectRoot } = require('./util')
-const { ens } = require('@aragon/aragen')
-const url = require('url')
+const { DEVCHAIN_ENS } = require('./commands/devchain_cmds/utils/constants')
 
 const debugMiddleware = argv => {
   argv.reporter.debug(
@@ -29,7 +29,7 @@ const cli = yargs
   .commandDir('./commands')
   .alias('env', 'environment')
   .alias('h', 'help')
-  // .alias('v', 'version') // TODO: Review if we can prevent clash with aragen verbose option
+  .alias('v', 'version')
   .epilogue('For more information, check out https://hack.aragon.org')
   .group(['help', 'version'], 'Global options:')
   // blank scriptName so that help text doesn't display "aragon" before each command
@@ -69,7 +69,7 @@ const cli = yargs
   .option('apm.ens-registry', {
     description:
       "Address of the ENS registry. This will be overwritten if the selected '--environment' from your arapp.json includes a `registry` property",
-    default: ens,
+    default: DEVCHAIN_ENS,
   })
   .group(['apm.ens-registry'], 'APM:')
   .option('apm.ipfs.rpc', {
