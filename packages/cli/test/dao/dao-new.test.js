5a1f0eeea10a3d5aab31411e8d5fc61c92155af5
Alejandro J. Santander <palebluedot@gmail.com>
2019-12-17 19:25:47 -0300

    Adds tests for the agent guide, dao new, and fixes a bug on act

--- packages/cli/test/dao/dao-new.test.js
+++ packages/cli/test/dao/dao-new.test.js
@@ -28,3 +28,23 @@ test('assigns an Aragon Id with the "--aragon-id" param', async t => {
   t.assert(daoId === id, 'Invalid Aragon Id')
   t.assert(/0x[a-fA-F0-9]{40}/.test(daoAddress), 'Invalid DAO address')
 })
+
+test('creates a new DAO with a custom template', async t => {
+  const { stdout } = await runAragonCLI([
+    'dao',
+    'new',
+    'membership-template',
+    '1.0.0',
+    '--fn', 'newTokenAndInstance',
+    '--fn-args',
+    'MyToken', 'TKN',
+    `MyDao${new Date().getTime()}`,
+    '[\"0xb4124cEB3451635DAcedd11767f004d8a28c6eE7\"]',
+    '[\"500000000000000000\", \"50000000000000000\", \"604800\"]',
+    '1296000',
+    'true'
+  ])
+  const daoAddress = stdout.match(daoAddressRegex)[1]
+
+  t.assert(/0x[a-fA-F0-9]{40}/.test(daoAddress), 'Invalid DAO address')
+})
