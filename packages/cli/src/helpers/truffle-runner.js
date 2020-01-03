ba8ac16df48cfb9460d12c897d0a82e7003d119d
Mathew Cormier <mathew.corm@gmail.com>
2020-01-03 15:59:53 -0500

    Run local truffle

--- packages/cli/src/helpers/truffle-runner.js
+++ packages/cli/src/helpers/truffle-runner.js
@@ -1,12 +1,9 @@
 import execa from 'execa'
 import devnull from 'dev-null'
-import { getBinary, getPackageRoot } from '@aragon/toolkit'
-
-const truffleBin = getBinary('truffle', getPackageRoot(__dirname))
 
 export const runTruffle = (args, { stdout, stderr, stdin }) => {
   return new Promise((resolve, reject) => {
-    const truffle = execa(truffleBin, args)
+    const truffle = execa('truffle', args, { preferLocal: true } )
     let errMsg = ''
     truffle.on('exit', code => {
       code === 0 ? resolve() : reject(errMsg)
