9d0c3b0c197ed09eb3547a4fba08616b5027da6f
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-07 14:46:55 -0300

    Basic Solidity watcher working

--- packages/buidler/contracts/Counter.sol
+++ packages/buidler/contracts/Counter.sol
@@ -4,17 +4,14 @@ import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/lib/math/SafeMath.sol";
 
 
-contract CounterApp is AragonApp {
+contract Counter is AragonApp {
     using SafeMath for uint256;
 
-    /// Events
     event Increment(address indexed entity, uint256 step);
     event Decrement(address indexed entity, uint256 step);
 
-    /// State
     uint256 public value;
 
-    /// ACL
     bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
     bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
 
@@ -22,21 +19,17 @@ contract CounterApp is AragonApp {
         initialized();
     }
 
-    /**
-     * @notice Increment the counter by `step`
-     * @param step Amount to increment by
-     */
     function increment(uint256 step) external auth(INCREMENT_ROLE) {
         value = value.add(step);
         emit Increment(msg.sender, step);
     }
 
-    /**
-     * @notice Decrement the counter by `step`
-     * @param step Amount to decrement by
-     */
     function decrement(uint256 step) external auth(DECREMENT_ROLE) {
         value = value.sub(step);
         emit Decrement(msg.sender, step);
+    }
+
+    function getVersion() public pure returns (string) {
+        return "0";
     }
 }
--- packages/buidler/tasks/start.ts
+++ packages/buidler/tasks/start.ts
@@ -4,43 +4,91 @@ import {
   TASK_START,
   TASK_START_NEW_DAO,
   TASK_START_NEW_APP_PROXY,
+  TASK_START_UPDATE_PROXY_IMPLEMENTATION,
+  TASK_START_WATCH_CONTRACTS
 } from './task-names'
 
+// import {
+//   TASK_COMPILE
+// } from '@nomiclabs/buidler/src/builtin-tasks/task-names'
+
+import filewatcher from 'filewatcher'
+
+const BASE_NAMESPACE = '0xf1f3eb40f5bc1ad1344716ced8b8a0431d840b5783aea1fd01786bc26f35ac0f';
+const APP_ID = '0xDEADBEEF';
+
 task(TASK_START, 'Starts Aragon app development')
-  .setAction(async ({}, { run }) => {
+  .setAction(async ({}, { web3, run }) => {
     console.log(`Starting...`);
 
+    // Define internal tasks.
     internalTask(TASK_START_NEW_DAO, newDao);
     internalTask(TASK_START_NEW_APP_PROXY, newAppProxy);
+    internalTask(TASK_START_UPDATE_PROXY_IMPLEMENTATION, updateProxyImplementation);
+    internalTask(TASK_START_WATCH_CONTRACTS, watchContracts)
 
-    const dao = await run(TASK_START_NEW_DAO);
+    // Compile contracts.
+    await run('compile')
+
+    // Retrieve active accounts.
+    const accounts = await web3.eth.getAccounts();
+    const root = accounts[0];
+
+    const dao = await run(TASK_START_NEW_DAO, { root });
     console.log(`New DAO created at: ${dao.address}`);
 
-    const proxy = await run(TASK_START_NEW_APP_PROXY, { dao })
-    console.log(`New proxy created at: ${proxy.address}`)
+    const proxy = await run(TASK_START_NEW_APP_PROXY, { root, dao });
+    console.log(`New app proxy created at: ${proxy.address}`);
+
+    await run(TASK_START_WATCH_CONTRACTS, { root, dao })
   });
 
-const newAppProxy = async ({ dao }, { web3, artifacts }) => {
-  // Retrieve active accounts.
-  const accounts = await web3.eth.getAccounts();
-  const root = accounts[0];
+const watchContracts = async ({ root, dao }, { run }) => {
+  console.log(`Watching for changes in contracts...`);
 
-  // Create app instance.
+  // Start the file watcher.
+  const watcher = filewatcher();
+  watcher.add('./contracts/Counter.sol');
+  watcher.on('change', async (file, stat) => {
+    console.log(`Contract changed: ${file}`);
+
+    await run('compile')
+
+    await run(TASK_START_UPDATE_PROXY_IMPLEMENTATION, { root, dao });
+  })
+
+  // Unresolving promise to keep task open.
+  return new Promise((resolve, reject) => {})
+}
+
+const updateProxyImplementation = async ({ root, dao }, { artifacts }) => {
+  console.log(`Updating implementation...`)
+
+  // Deploy base implementation.
   const App = artifacts.require('Counter');
-  const appBase = await App.new();
+  const implementation = await App.new();
+  console.log(`  New implementation: ${ implementation.address }`)
+
+  // Set the new implementation in the Kernel.
+  await dao.setApp(BASE_NAMESPACE, APP_ID, implementation.address, { from: root })
+}
+
+const newAppProxy = async ({ root, dao }, { artifacts }) => {
+  // Deploy base implementation.
+  const App = artifacts.require('Counter');
+  const implementation = await App.new();
+  console.log(`  First implementation: ${ implementation.address }`)
+
+  // Create a new app proxy with base implementation.
+  const { logs } = await dao.newAppInstance(APP_ID, implementation.address, '0x', false, { from: root });
 
   // Retrieve proxy address and wrap around abi.
-  const { logs } = await dao.newAppInstance('0xDeAdBeEf', appBase.address, '0x', false, { from: root });
   const proxy = App.at(logs.find(l => l.event === 'NewAppProxy').args.proxy);
 
   return proxy;
 };
 
-const newDao = async ({}, { web3, artifacts }) => {
-  // Retrieve active accounts.
-  const accounts = await web3.eth.getAccounts();
-  const root = accounts[0];
-
+const newDao = async ({ root }, { artifacts }) => {
   // Create Kernel instance.
   const Kernel = artifacts.require('Kernel');
   const dao = await Kernel.new(false);
@@ -50,7 +98,7 @@ const newDao = async ({}, { web3, artifacts }) => {
   const aclBase = await ACL.new();
   await dao.initialize(aclBase.address, root);
 
-  // Give first account the APP_MANAGER_ROLE.
+  // Give first account the ability to manage apps.
   const acl = await ACL.at(await dao.acl());
   await acl.createPermission(
     root,
