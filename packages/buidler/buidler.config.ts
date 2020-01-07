e315329eec2eb35fa8f71d666246f22797c0d35d
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-07 11:35:38 -0300

    Basics of start compound task

--- packages/buidler/buidler.config.ts
+++ packages/buidler/buidler.config.ts
@@ -3,17 +3,21 @@ import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config';
 usePlugin('@nomiclabs/buidler-truffle5');
 usePlugin('@nomiclabs/buidler-web3');
 
-import './tasks/new_dao';
+import './tasks/start';
 
 const config: BuidlerConfig = {
   defaultNetwork: 'localhost',
   networks: {
     localhost: {
-      url: 'http://localhost:8545'
+      url: 'http://localhost:8545',
+      accounts: [
+        "0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563",
+        "0xce8e3bda3b44269c147747a373646393b1504bfcbb73fc9564f5d753d8116608"
+      ]
     }
   },
   solc: {
-    version: '0.5.12',
+    version: '0.4.24',
   },
 };
 
--- packages/buidler/tasks/task-names.ts
+++ packages/buidler/tasks/task-names.ts
@@ -1,1 +1,3 @@
-export const TASK_NEW_DAO = "new_dao";
+export const TASK_START = 'start';
+export const TASK_START_NEW_DAO = "start:new-dao";
+export const TASK_START_NEW_APP_PROXY = "start:new-app-proxy";
