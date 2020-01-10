511058dfb7a87b27295d014dca91a0ac33e99fba
dappLion <35266934+dapplion@users.noreply.github.com>
2020-01-10 20:20:49 +0100

    Add front-end utils (#1193)

--- packages/buidler/buidler.config.ts
+++ packages/buidler/buidler.config.ts
@@ -4,6 +4,7 @@ usePlugin('@nomiclabs/buidler-truffle5');
 usePlugin('@nomiclabs/buidler-web3');
 
 import './tasks/start';
+import './tasks/start-frontend';
 
 const config: BuidlerConfig = {
   defaultNetwork: 'localhost',
@@ -11,14 +12,14 @@ const config: BuidlerConfig = {
     localhost: {
       url: 'http://localhost:8545',
       accounts: [
-        "0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563",
-        "0xce8e3bda3b44269c147747a373646393b1504bfcbb73fc9564f5d753d8116608"
+        '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
+        '0xce8e3bda3b44269c147747a373646393b1504bfcbb73fc9564f5d753d8116608'
       ]
     }
   },
   solc: {
-    version: '0.4.24',
-  },
+    version: '0.4.24'
+  }
 };
 
 export default config;
--- packages/buidler/tasks/task-names.ts
+++ packages/buidler/tasks/task-names.ts
@@ -1,6 +1,9 @@
 // Aragon plugin tasks.
 export const TASK_START = 'start';
 export const TASK_START_WATCH_CONTRACTS = 'start:watch-contracts';
+export const TASK_START_WATCH_FRONTEND = 'start:watch-frontend';
 
 // Buidler built-in tasks.
 export const TASK_COMPILE = 'compile';
+export const TASK_FLATTEN_GET_FLATTENED_SOURCE =
+  'flatten:get-flattened-sources';
