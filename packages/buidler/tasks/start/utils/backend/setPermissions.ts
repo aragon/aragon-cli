f18170a3728e722e8f88ae8aab4fac7f3c1084bd
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-14 13:06:19 -0300

    Typed setPermissions

--- packages/buidler/tasks/start/utils/backend/setPermissions.ts
+++ packages/buidler/tasks/start/utils/backend/setPermissions.ts
@@ -1,26 +1,46 @@
-const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff';
+import { TruffleEnvironmentArtifacts } from '@nomiclabs/buidler-truffle5/src/artifacts';
 
-async function setPermissions(dao, app, root, artifacts) {
+import {
+  KernelInstance,
+  ACLInstance
+} from '../../../../typechain';
+
+const ANY_ADDRESS: string = '0xffffffffffffffffffffffffffffffffffffffff';
+
+async function setPermissions(
+  dao: KernelInstance,
+  app: any, // TODO: needs type
+  rootAddress: string,
+  artifacts: TruffleEnvironmentArtifacts
+): Promise<void> {
   // Retrieve ACL.
   const aclAddress = await dao.acl();
   const ACL = artifacts.require('ACL');
   const acl = await ACL.at(aclAddress);
 
+  // Set all permissions listed in arapp.json.
   // TODO: Must be set dynamically.
-  await setPermission(acl, app, await app.INCREMENT_ROLE(), root);
-  await setPermission(acl, app, await app.DECREMENT_ROLE(), root);
+  await setOpenPermission(acl, app, await app.INCREMENT_ROLE(), rootAddress);
+  await setOpenPermission(acl, app, await app.DECREMENT_ROLE(), rootAddress);
 }
 
-async function setPermission(acl, app, permission, root) {
+async function setOpenPermission(
+  acl: ACLInstance,
+  app: Truffle.Contract<any>,
+  permission: string,
+  rootAddress: string
+) {
   console.log(`Setting permission: ${permission}`)
 
+  // Set permission to ANY_ADDRESS (max uint256), which is interpreted by
+  // the ACL as giving such permission to all addresses.
   await acl.createPermission(
     ANY_ADDRESS,
     app.address,
     permission,
-    root,
-    { from: root }
-  )
+    rootAddress,
+    { from: rootAddress }
+  );
 }
 
 export default setPermissions;
