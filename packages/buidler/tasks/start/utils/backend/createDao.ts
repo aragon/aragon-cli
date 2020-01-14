285309528f6455b9ba0e10b9b3a746b8bb95b329
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-14 12:16:20 -0300

    Typed createDao.ts

--- packages/buidler/tasks/start/utils/backend/createDao.ts
+++ packages/buidler/tasks/start/utils/backend/createDao.ts
@@ -1,34 +1,58 @@
+/// <reference types="truffle-typings" />
+
+import { TruffleEnvironmentArtifacts } from '@nomiclabs/buidler-truffle5/src/artifacts';
+
+import {
+  KernelContract, KernelInstance,
+  ACLContract, ACLInstance,
+  DAOFactoryContract, DAOFactoryInstance,
+  EVMScriptRegistryFactoryContract, EVMScriptRegistryFactoryInstance
+} from '../../../../typechain';
+
 /**
  * Deploys a new DAO
  * @return DAO's Kernel TruffleContract
  */
-async function createDao(root, artifacts) {
+async function createDao(rootAddress: string, artifacts: TruffleEnvironmentArtifacts): Promise<KernelInstance> {
+  // Retrieve contract artifacts.
+  const Kernel: KernelContract = artifacts.require('Kernel');
+  const ACL: ACLContract = artifacts.require('ACL');
+  const DAOFactory: DAOFactoryContract = artifacts.require('DAOFactory');
+  const EVMScriptRegistryFactory: EVMScriptRegistryFactoryContract = artifacts.require('EVMScriptRegistryFactory');
+
   // Deploy a DAOFactory.
-  const Kernel = artifacts.require('Kernel');
-  const kernelBase = await Kernel.new(true /*petrifyImmediately*/);
-  const ACL = artifacts.require('ACL');
-  const aclBase = await ACL.new();
-  const EVMScriptRegistryFactory = artifacts.require('EVMScriptRegistryFactory');
-  const registryFactory = await EVMScriptRegistryFactory.new();
-  const DAOFactory = artifacts.require('DAOFactory');
-  const daoFactory = await DAOFactory.new(
+  const kernelBase: KernelInstance = await Kernel.new(true /*petrifyImmediately*/);
+  const aclBase: ACLInstance = await ACL.new();
+  const registryFactory: EVMScriptRegistryFactoryInstance = await EVMScriptRegistryFactory.new();
+  const daoFactory: DAOFactoryInstance = await DAOFactory.new(
     kernelBase.address,
     aclBase.address,
     registryFactory.address
   );
 
   // Create a DAO instance using the factory.
-  const { logs } = await daoFactory.newDAO(root);
-  const dao = await Kernel.at(logs.find(l => l.event === 'DeployDAO').args.dao);
+  const txResponse: Truffle.TransactionResponse = await daoFactory.newDAO(rootAddress);
 
-  // Give root the ability to manage apps.
-  const acl = await ACL.at(await dao.acl());
+  // Find the created DAO instance address from the transaction logs.
+  const logs: Truffle.TransactionLog[] = txResponse.logs;
+  const log: Truffle.TransactionLog | undefined = logs.find(l => l.event === 'DeployDAO');
+  if (!log) {
+    throw new Error('Error deploying new DAO. Unable to find DeployDAO log.');
+  }
+  const daoAddress: string = log.args.dao;
+
+  // Use the DAO address to construct a full KernelInstance object.
+  const dao: KernelInstance = await Kernel.at(daoAddress);
+
+  // Give rootAddress the ability to manage apps.
+  const aclAddress: string = await dao.acl();
+  const acl: ACLInstance = await ACL.at(aclAddress);
   await acl.createPermission(
-    root,
+    rootAddress,
     dao.address,
     await dao.APP_MANAGER_ROLE(),
-    root,
-    { from: root }
+    rootAddress,
+    { from: rootAddress }
   );
 
   return dao;
