d3c43a3606666f1dc24eb2c9a391e44ebc9a4d3f
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-20 13:14:09 -0300

    Updated permissions tests to use changes to setAllPermissionsOpenly

--- packages/buidler/test/tasks/start/utils/backend/permissions.test.ts
+++ packages/buidler/test/tasks/start/utils/backend/permissions.test.ts
@@ -1,7 +1,9 @@
 import { assert } from 'chai'
 import { createDao } from '~/src/tasks/start/utils/backend/dao'
+import { readArapp } from '~/src/tasks/start/utils/arapp'
 import { deployImplementation } from '~/src/tasks/start/utils/backend/app'
 import { createProxy } from '~/src/tasks/start/utils/backend/proxy'
+import { AragonAppJson } from '~/src/types'
 import {
   setAllPermissionsOpenly,
   ANY_ADDRESS
@@ -18,6 +20,7 @@ describe('permissions.ts', () => {
   let dao: KernelInstance
   let acl: ACLInstance
   let app: CounterInstance
+  let arapp: AragonAppJson
 
   before('set up dao with app', async () => {
     dao = await createDao()
@@ -28,11 +31,13 @@ describe('permissions.ts', () => {
     const implementation = await deployImplementation()
     const appId = getAppId('counter')
     app = (await createProxy(implementation, appId, dao)) as CounterInstance
+
+    arapp = readArapp()
   })
 
   describe('when setAllPermissionsOpenly is called', () => {
     before('call setAllPermissionsOpenly', async () => {
-      await setAllPermissionsOpenly(dao, app)
+      await setAllPermissionsOpenly(dao, app, arapp)
     })
 
     it('properly sets the INCREMENT_ROLE permission', async () => {
