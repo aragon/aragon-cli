76d1e19593b906927fec87177f7e4c896b097d71
Alejandro J. Santander <palebluedot@gmail.com>
2020-01-21 12:15:48 -0300

    Using path.join

--- packages/buidler/src/index.ts
+++ packages/buidler/src/index.ts
@@ -1,5 +1,6 @@
 import { extendConfig, usePlugin } from '@nomiclabs/buidler/config'
 import defaultAragonConfig from './config'
+import path from 'path'
 // TODO: Don't use any type below, try to use something like these...
 // import { ResolvedBuidlerConfig, BuidlerConfig } from '@nomiclabs/buidler/types'
 
@@ -9,7 +10,7 @@ export default function(): void {
   usePlugin('@nomiclabs/buidler-web3')
 
   // Task definitions.
-  require(__dirname + '/tasks/start/start-task')
+  require(path.join(__dirname, '/tasks/start/start-task'))
 
   // Environment extensions.
   // No extensions atm.
