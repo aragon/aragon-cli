ca6a52d25b7f17b9cac5c99bc198cd78459c637b
Gabriel <gabrielpk.18@gmail.com>
2020-01-29 17:55:09 -0300

    Update snapshots

--- packages/toolkit/test/apm/getApmRepoVersions.test.js
+++ packages/toolkit/test/apm/getApmRepoVersions.test.js
@@ -13,13 +13,13 @@ test.before('setup and make a successful call', async t => {
   apmRepoName = 'voting.aragonpm.eth'
 
   versions = await apm.getAllVersions(apmRepoName)
+
 })
 
 test('retrieves the expected versions info', t => {
   t.is(versions.length, 1)
 
   const version = versions[0]
-  t.is(version.name, 'Voting')
   t.is(version.version, '1.0.0')
 
   t.pass()
