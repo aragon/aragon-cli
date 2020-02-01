e64bed10137cf119b13e66073a53f27f25ef4c70
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-02-01 23:03:26 +0200

    refactored testing of ipfs start/stop commands from cli

--- packages/cli/test/ipfs/ipfs-start.test.js
+++ packages/cli/test/ipfs/ipfs-start.test.js
@@ -1,57 +1,93 @@
 import test from 'ava'
+import { remove } from 'fs-extra'
 import parseCli from '../parseCli'
+import { initPackage, installGoIpfs, killProcessOnPort } from '@aragon/toolkit'
+
+// sandbox project with local ipfs
+const projectPath = './.tmp/test_cli_ipfs/project'
+const repoPath = './.tmp/test_cli_ipfs/repo/'
+const binPath = './.tmp/test_cli_ipfs/project/node_modules/.bin/ipfs'
+
+const apiPort = 18080
+const swarmPort = 14001
+const gatewayPort = 15001
+
+const repoPathOpt = `--repo-path ${repoPath}`
+const binPathOpt = `--bin-path ${binPath}`
+const apiPortOpt = `--api-port ${apiPort}`
+const swarmPortOpt = `--swarm-port ${swarmPort}`
+const gatewayPortOpt = `--gateway-port ${gatewayPort}`
+
+test.serial.before(async t => {
+  // setup local ipfs
+  await initPackage(projectPath)
+  await installGoIpfs(true, projectPath)
+})
 
 test.serial.beforeEach(async t => {
-  // stop ipfs before each test
-  await stopIpfsCommand()
+  // stop local ipfs before each test
+  await killProcessOnPort(apiPort)
 })
 
 test.serial.afterEach(async t => {
-  // stop ipfs after each test
-  await stopIpfsCommand()
+  // stop local ipfs after each test
+  await killProcessOnPort(apiPort)
 })
 
-test.serial.after(async t => {
-  // start the ipfs daemon again, prevent breaking of following tests
-  await parseCli(['ipfs', 'start', '--debug', '-D'])
+test.serial.after.always(async t => {
+  // cleanup
+  await killProcessOnPort(apiPort)
+  await remove(projectPath)
+  await remove(repoPath)
 })
 
-test.serial('ipfs start successfully starts daemon', async t => {
-  let output = null
-  // starting ipfs daemon in background
-  output = await parseCli(['ipfs', 'start', '--debug', '-D'])
+test.serial('ipfs start successfully starts the daemon', async t => {
+  // starting local ipfs daemon in background
+  let output = await parseCli(
+    `ipfs start --debug -D ${repoPathOpt} ${binPathOpt} ${apiPortOpt} ${swarmPortOpt} ${gatewayPortOpt}`
+  )
 
-  // test that ipfs daemon succesfully started
-  output = await parseCli(['ipfs', 'status', '--debug'])
-  t.assert(output.includes('Daemon: running'))
+  t.true(output.includes(`Swarm listening on /ip4/127.0.0.1/tcp/${swarmPort}`))
+  t.true(output.includes(`Swarm announcing /ip4/127.0.0.1/tcp/${swarmPort}`))
+  t.true(output.includes(`API server listening on /ip4/0.0.0.0/tcp/${apiPort}`))
+  t.true(
+    output.includes(
+      `Gateway (readonly) server listening on /ip4/0.0.0.0/tcp/${gatewayPort}`
+    )
+  )
+
+  // get the status
+  output = await parseCli(`ipfs status --debug  ${repoPathOpt}`)
+  t.true(output.includes('Daemon: running'))
 })
 
 test.serial('ipfs start throws error if daemon already running', async t => {
-  // start ipfs daemon
-  await parseCli(['ipfs', 'start', '--debug', '-D'])
+  // starting local ipfs daemon in background
+  const output = await parseCli(
+    `ipfs start --debug -D ${repoPathOpt} ${binPathOpt} ${apiPortOpt} ${swarmPortOpt} ${gatewayPortOpt}`
+  )
+  t.true(output.includes('Daemon is ready'))
 
   // start again ipfs daemon, expect exception is thrown
-  await t.throwsAsync(async () => parseCli(['ipfs', 'start', '--debug', '-D']))
+  await t.throwsAsync(async () =>
+    parseCli(
+      `ipfs start --debug -D ${repoPathOpt} ${binPathOpt} ${apiPortOpt} ${swarmPortOpt} ${gatewayPortOpt}`
+    )
+  )
 })
 
 test.serial('ipfs stop successfully kills the daemon', async t => {
   let output = null
-  // starting ipfs daemon in background
-  output = await parseCli(['ipfs', 'start', '--debug', '-D'])
-  t.assert(output.includes('Daemon is ready'))
+  // starting local ipfs daemon in background
+  output = await parseCli(
+    `ipfs start --debug -D ${repoPathOpt} ${binPathOpt} ${apiPortOpt} ${swarmPortOpt} ${gatewayPortOpt}`
+  )
+  t.true(output.includes('Daemon is ready'))
 
   // stop the daemon
-  await parseCli(['ipfs', 'stop', '--debug'])
+  await parseCli(`ipfs stop --debug ${repoPathOpt}`)
 
   // test that ipfs is succesfully stopped
-  output = await parseCli(['ipfs', 'status', '--debug'])
-  t.assert(output.includes('Daemon: stopped'))
+  output = await parseCli(`ipfs status --debug ${repoPathOpt}`)
+  t.true(output.includes('Daemon: stopped'))
 })
-
-async function stopIpfsCommand() {
-  try {
-    await parseCli(['ipfs', 'stop', '--debug'])
-  } catch (e) {
-    /* We reach here ipfs is already running */
-  }
-}
