e5dbe04fa770ab7987b6ab7bbd1803e197daa094
Mathew Cormier <mathew.corm@gmail.com>
2020-02-13 17:48:06 -0500

    Lint fix

--- packages/toolkit/test/node/process.test.js
+++ packages/toolkit/test/node/process.test.js
@@ -1,50 +1,48 @@
 import test from 'ava'
 //
-import { startProcess, } from '../../src/node/process'
-
+import { startProcess } from '../../src/node/process'
 
 test('startProcess should enable detach', async t => {
-    const { detach } = await startProcess({
-        cmd: 'aragon',
-        args: ['--version'],
-        readyOutput: '.',
-        execaOpts: {
-            detached: true,
-        },
-        timeout: 30000
-    })
+  const { detach } = await startProcess({
+    cmd: 'aragon',
+    args: ['--version'],
+    readyOutput: '.',
+    execaOpts: {
+      detached: true,
+    },
+    timeout: 30000,
+  })
 
-    detach()
-    t.pass()
+  detach()
+  t.pass()
 })
 
 test('startProcess should enable kill', async t => {
-    const { kill } = await startProcess({
-        cmd: 'aragon',
-        args: ['--version'],
-        readyOutput: '.',
-        execaOpts: {
-            detached: true,
-        },
-        timeout: 30000
-    })
+  const { kill } = await startProcess({
+    cmd: 'aragon',
+    args: ['--version'],
+    readyOutput: '.',
+    execaOpts: {
+      detached: true,
+    },
+    timeout: 30000,
+  })
 
-    kill()
-    t.pass()
+  kill()
+  t.pass()
 })
 
 test('startProcess should support attach', async t => {
-    const { attach } = await startProcess({
-        cmd: 'aragon',
-        args: ['--help'],
-        readyOutput: 'Usage',
-        execaOpts: {
-            detached: true,
-        },
-        timeout: 30000
-    })
+  const { attach } = await startProcess({
+    cmd: 'aragon',
+    args: ['--help'],
+    readyOutput: 'Usage',
+    execaOpts: {
+      detached: true,
+    },
+    timeout: 30000,
+  })
 
-    attach()
-    t.pass()
+  attach()
+  t.pass()
 })
-
--- packages/toolkit/test/util.test.js
+++ packages/toolkit/test/util.test.js
@@ -1,7 +1,12 @@
 import test from 'ava'
 //
-import { isAddress, convertDAOIdToSubdomain, getIpfsCacheFiles, getAragonGanacheFiles, expandLink } from '../src/util'
-
+import {
+  isAddress,
+  convertDAOIdToSubdomain,
+  getIpfsCacheFiles,
+  getAragonGanacheFiles,
+  expandLink,
+} from '../src/util'
 
 test('isAddress returns the correct value', t => {
   t.true(isAddress('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7'))
@@ -27,13 +32,12 @@ test('getAragonGanacheFiles returns the correct path', t => {
 
 test('expandLink returns the correct values', t => {
   const link = expandLink({
-    name: 'test', 
-    address: '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7'
+    name: 'test',
+    address: '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7',
   })
 
   t.is(link.name, 'test')
   t.is(link.address, '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7')
   t.is(link.placeholder, '__test__________________________________')
   t.is(link.addressBytes, 'b4124cEB3451635DAcedd11767f004d8a28c6eE7')
-
 })
