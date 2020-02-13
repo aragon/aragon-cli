a593c5f02735f92634950c28fc2fe3287640a678
Mathew Cormier <mathew.corm@gmail.com>
2020-02-13 17:44:52 -0500

    Increase process coverage

---
+++ packages/toolkit/test/node/process.test.js
@@ -0,0 +1,50 @@
+import test from 'ava'
+//
+import { startProcess, } from '../../src/node/process'
+
+
+test('startProcess should enable detach', async t => {
+    const { detach } = await startProcess({
+        cmd: 'aragon',
+        args: ['--version'],
+        readyOutput: '.',
+        execaOpts: {
+            detached: true,
+        },
+        timeout: 30000
+    })
+
+    detach()
+    t.pass()
+})
+
+test('startProcess should enable kill', async t => {
+    const { kill } = await startProcess({
+        cmd: 'aragon',
+        args: ['--version'],
+        readyOutput: '.',
+        execaOpts: {
+            detached: true,
+        },
+        timeout: 30000
+    })
+
+    kill()
+    t.pass()
+})
+
+test('startProcess should support attach', async t => {
+    const { attach } = await startProcess({
+        cmd: 'aragon',
+        args: ['--help'],
+        readyOutput: 'Usage',
+        execaOpts: {
+            detached: true,
+        },
+        timeout: 30000
+    })
+
+    attach()
+    t.pass()
+})
+
