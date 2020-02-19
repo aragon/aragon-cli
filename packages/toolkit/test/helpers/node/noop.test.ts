28c8c5b142a8cc7a8649db26fb646139f7338a40
dapplion <guillem.lion.benosa@gmail.com>
2020-02-19 20:25:24 +0800

    Add ts-node to support types in tests

--- packages/toolkit/test/helpers/node/noop.test.ts
+++ packages/toolkit/test/helpers/node/noop.test.ts
@@ -4,3 +4,12 @@ import { noop } from '../../../src'
 test('noop() returns undefined', t => {
   t.is(noop(), undefined)
 })
+
+test('types are working', t => {
+  interface Person {
+    name: string
+    lastName: string
+  }
+  const person: Person = { name: 'Joe', lastName: 'Doe' }
+  t.is(person.name, 'Joe')
+})
