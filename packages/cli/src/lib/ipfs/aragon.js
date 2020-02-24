c9fa68969606e44164616f9b6fc0fa351ec485f9
Gabriel <gabrielpk.18@gmail.com>
2020-02-24 15:15:13 -0300

    Update shrinkwraps

--- packages/cli/src/lib/ipfs/aragon.js
+++ packages/cli/src/lib/ipfs/aragon.js
@@ -2,7 +2,6 @@ import { getIpfsCacheFiles } from '@aragon/toolkit'
 
 export const pinArtifacts = async httpClient => {
   const files = getIpfsCacheFiles()
-  console.log('>>filesPath:', files)
   return httpClient.addFromFs(files, {
     recursive: true,
     ignore: 'node_modules',
