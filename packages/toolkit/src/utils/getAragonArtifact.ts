0184c332c0b2cd4438bf2fe215195f843d2b91dd
Gabriel <gabrielpk.18@gmail.com>
2020-02-26 15:47:12 -0300

    Cherry pick parser from previous PR and apply some types fixes

---
+++ packages/toolkit/src/utils/getAragonArtifact.ts
@@ -0,0 +1,48 @@
+import { mapValues } from 'lodash'
+import { AragonAppJson, AragonArtifact } from '../types'
+import { keccak256, AbiItem } from 'web3-utils'
+import { AragonContractFunction } from '../utils/parseContractFunctions'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const namehash = require('eth-ens-namehash')
+
+export default function getAragonArtifact(
+  arapp: AragonAppJson,
+  sourceCode: string,
+  functions: AragonContractFunction[],
+  abi: AbiItem[]
+): AragonArtifact {
+  return {
+    ...arapp,
+
+    // Artifact appears to require the appId of each environment
+    environments: mapValues(arapp.environments, environment => ({
+      ...environment,
+      appId: namehash.hash(environment.appName),
+    })),
+
+    // Artifact appears to require the abi of each function
+    functions: functions.map(fn => ({
+      // #### Todo: Is the signature actually necessary?
+      roles: fn.roles.map(role => role.id),
+      notice: fn.notice,
+      abi: abi.find(
+        abiElem =>
+          abiElem.type === 'function' &&
+          abiElem.name === fn.name &&
+          abiElem.inputs &&
+          abiElem.inputs.length === fn.paramTypes.length
+      ),
+    })),
+
+    // Artifact appears to require the roleId to have bytes precomputed
+    roles: (arapp.roles || []).map(role => ({
+      ...role,
+      bytes: keccak256(role.id),
+    })),
+
+    abi,
+    // Additional metadata
+    flattenedCode: `./code.sol`,
+  }
+}
---
+++ packages/toolkit/test/utils/getAragonArtifact.test.ts
@@ -0,0 +1,104 @@
+import test from 'ava'
+import { AragonAppJson } from '../../src/types'
+import { AragonContractFunction } from '../../src/utils/parseContractFunctions'
+import { AbiItem } from 'web3-utils'
+//
+import getAragonArtifact from '../../src/utils/getAragonArtifact'
+
+/* Tests */
+test('getAragonArtifact', t => {
+  const arapp: AragonAppJson = {
+    environments: {
+      mainnet: {
+        registry: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+        appName: 'finance.aragonpm.eth',
+        network: 'mainnet',
+      },
+    },
+    roles: [
+      {
+        name: 'Create new payments',
+        id: 'CREATE_PAYMENTS_ROLE',
+        params: [
+          'Token address',
+          'Receiver address',
+          'Token amount',
+          'Payment interval',
+          'Max repeats',
+          'Initial payment time',
+        ],
+      },
+    ],
+    path: 'contracts/Finance.sol',
+  }
+
+  const functions: AragonContractFunction[] = [
+    {
+      name: 'newImmediatePayment',
+      notice: '',
+      paramTypes: ['address', 'address', 'uint256', 'string'],
+      roles: [{ id: 'CREATE_PAYMENTS_ROLE', paramCount: 6 }],
+    },
+  ]
+
+  const abi: AbiItem[] = [
+    {
+      constant: false,
+      inputs: [
+        { name: '_token', type: 'address' },
+        { name: '_receiver', type: 'address' },
+        { name: '_amount', type: 'uint256' },
+        { name: '_reference', type: 'string' },
+      ],
+      name: 'newImmediatePayment',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ]
+
+  const artifact = getAragonArtifact(arapp, functions, abi)
+
+  console.log(JSON.stringify(artifact, null, 2))
+
+  t.deepEqual(artifact, {
+    environments: {
+      mainnet: {
+        registry: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+        appName: 'finance.aragonpm.eth',
+        network: 'mainnet',
+        appId:
+          '0xbf8491150dafc5dcaee5b861414dca922de09ccffa344964ae167212e8c673ae',
+      },
+    },
+    roles: [
+      {
+        name: 'Create new payments',
+        id: 'CREATE_PAYMENTS_ROLE',
+        params: [
+          'Token address',
+          'Receiver address',
+          'Token amount',
+          'Payment interval',
+          'Max repeats',
+          'Initial payment time',
+        ],
+        bytes:
+          '0x5de467a460382d13defdc02aacddc9c7d6605d6d4e0b8bd2f70732cae8ea17bc',
+      },
+    ],
+    functions: [],
+    path: 'contracts/Finance.sol',
+    abi,
+    appName: 'finance.aragonpm.eth',
+    env: {
+      registry: '0x314159265dd8dbb310642f98f50c066173c1259b',
+      appName: 'finance.aragonpm.eth',
+      network: 'mainnet',
+    },
+    appId: '0xbf8491150dafc5dcaee5b861414dca922de09ccffa344964ae167212e8c673ae',
+    // Additional metadata for accountability
+    flattenedCode: './code.sol',
+  })
+})
