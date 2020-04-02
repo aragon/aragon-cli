5af55fd727032a6a6835e5961b62361c3256f72d
Mathew Cormier <mathew.corm@gmail.com>
2020-04-02 14:32:09 -0400

    Add revokePermission function

---
+++ packages/toolkit/src/apm/revokePermission.js
@@ -0,0 +1,48 @@
+import APM from '@aragon/apm'
+import ACL from './util/acl'
+
+export default async (
+  web3,
+  apmRepoName,
+  apmOptions,
+  entity,
+  progressHandler = () => {},
+  { gasPrice }
+) => {
+  if (entity.length === 0) {
+    throw new Error('No addresse provided')
+  }
+
+  const apm = await APM(web3, apmOptions)
+  const acl = ACL(web3)
+
+  progressHandler(1)
+
+  const repo = await apm.getRepository(apmRepoName)
+  if (repo === null) {
+    throw new Error(
+      `Repository ${apmRepoName} does not exist and it's registry does not exist`
+    )
+  }
+
+  const receipts = []
+
+  /* eslint-disable-next-line */
+  progressHandler(2, address)
+
+  // Decode sender
+  const accounts = await web3.eth.getAccounts()
+  const from = accounts[0]
+
+  // Build transaction
+  const transaction = await acl.revoke(repo.options.address, address, from)
+
+  transaction.from = from
+  transaction.gasPrice = gasPrice
+  // the recommended gasLimit is already calculated by the ACL module
+
+  const receipt = await web3.eth.sendTransaction(transaction)
+  progressHandler(3, receipt.transactionHash)
+
+  return receipt
+}
