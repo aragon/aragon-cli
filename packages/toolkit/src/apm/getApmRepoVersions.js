7b09684f4681eda356e290a674b4db6d157a6059
Mathew Cormier <mathew.corm@gmail.com>
2020-06-03 20:00:47 -0400

    Increase timeout in getApmRepoVersions

--- packages/toolkit/src/apm/getApmRepoVersions.js
+++ packages/toolkit/src/apm/getApmRepoVersions.js
@@ -1,11 +1,12 @@
 import APM from '@aragon/apm'
+import { DEFAULT_IPFS_TIMEOUT } from '../helpers/constants'
 
 export default async (web3, apmRepoName, apmOptions) => {
   // Prepare APM object that can comunicate with the apm contracts.
   const apm = await APM(web3, apmOptions)
 
   // Query all versions for this repo.
-  const versions = await apm.getAllVersions(apmRepoName)
+  const versions = await apm.getAllVersions(apmRepoName, DEFAULT_IPFS_TIMEOUT)
 
   return versions
 }
