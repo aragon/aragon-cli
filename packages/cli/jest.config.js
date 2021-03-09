cfc349b460930aee0d1399519ddc24eec7b1534d
Nestor Barboza <nestorbarboza@gmail.com>
2021-03-08 20:13:32 -0500

    cleared jest.config files & added scripts flags

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -4,26 +4,14 @@
  */
 
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'babel',
 
-  // Reset the module registry before running each individual test
-  resetModules: true,
-
   // The test environment that will be used for testing
   testEnvironment: 'node',
-
-  // Indicates whether each individual test should be reported during the run
-  verbose: true,
-
-  // Whether to use watchman for file crawling
-  watchman: false,
 
   // Jest Runner
   runner: 'jest-serial-runner',
