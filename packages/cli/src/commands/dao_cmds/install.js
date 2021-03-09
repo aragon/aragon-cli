3a3ee8d52e04da97ddfe18e433680a950cba87df
Nestor Barboza <nestorbarboza@gmail.com>
2021-03-08 21:25:02 -0500

    added userHasCreatePermissionRole to fix dao install error message

--- packages/cli/src/commands/dao_cmds/install.js
+++ packages/cli/src/commands/dao_cmds/install.js
@@ -16,6 +16,7 @@ import {
   getDefaultRepoPath,
   isLocalDaemonRunning,
   getApmRepo,
+  userHasCreatePermissionRole,
 } from '@aragon/toolkit'
 //
 import { ensureWeb3 } from '../../helpers/web3-fallback'
@@ -75,6 +76,16 @@ export const handler = async function ({
     web3,
     apmOptions.ensRegistryAddress
   )
+
+  const accounts = await web3.eth.getAccounts()
+  const userAddress = accounts[0]
+  if (!userAddress) throw Error(`Error getting accounts from web3`)
+
+  // Verify that user has permissions to install
+  if (!(await userHasCreatePermissionRole(dao, userAddress, web3)))
+    throw Error(
+      `This address ${userAddress} does not have permission to install ${apmRepoName}`
+    )
 
   const tasks = new TaskList(
     [
