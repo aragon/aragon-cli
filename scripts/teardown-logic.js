c7ef3cb11a5bba48df936201960deb6976cd13c6
Nathanael Mueller <inathan.m@gmail.com>
2021-03-12 03:57:59 -0800

    fix: teardown scripts and timeout

---
+++ scripts/teardown-logic.js
@@ -0,0 +1,25 @@
+const { killProcessOnPort } = require('../packages/toolkit')
+
+const IPFS_API_PORT = 5001
+const DEVCHAIN_PORT = 8545
+
+const LOGGER_PREFIX = '[cli-monorepo:posttest]'
+
+const logger = (...messages) => {
+  console.log(LOGGER_PREFIX, ...messages)
+}
+
+module.exports = function() {
+  Promise.all([
+    killProcessOnPort(IPFS_API_PORT),
+    killProcessOnPort(DEVCHAIN_PORT),
+  ])
+    .then(() => {
+      logger(`Processes killed on ports: ${IPFS_API_PORT}, ${DEVCHAIN_PORT}`)
+    })
+    .catch(() => {
+      logger(
+        `Cannot kill processes on ports: ${IPFS_API_PORT}, ${DEVCHAIN_PORT}}`
+      )
+    })
+}
