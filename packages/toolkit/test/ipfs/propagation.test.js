0dd5a45e5d58c8f9d274eb3b92a9c8379c8153e6
Nathanael Mueller <inathan.m@gmail.com>
2021-03-16 15:30:13 -0700

    fix: ava references removed, gitignore updated

--- packages/toolkit/test/ipfs/propagation.test.js
+++ packages/toolkit/test/ipfs/propagation.test.js
@@ -1,6 +1,5 @@
 import { propagateFiles } from '../../src/ipfs'
 
-// eslint-disable-next-line ava/no-skip-test
 test.skip('Get IPFS readme merkle DAG and CIDs', async () => {
   const ipfsGateway = 'https://ipfs.eth.aragon.network/ipfs'
   const readmeDirCid = 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
