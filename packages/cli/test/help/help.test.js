a9babdd362f990283bf7a7da2489c33352cac147
c0rv0s <inathan.m@gmail.com>
2021-03-23 23:10:23 -0700

    fix: timeout for help test

--- packages/cli/test/help/help.test.js
+++ packages/cli/test/help/help.test.js
@@ -1,6 +1,7 @@
 import { normalizeOutput } from '../../src/util'
 import { runAragonCLI } from '../util'
 
+jest.setTimeout(20000)
 test('should return the correct help info', async () => {
   let { stdout } = await runAragonCLI(['--help'])
   stdout = normalizeOutput(stdout)
